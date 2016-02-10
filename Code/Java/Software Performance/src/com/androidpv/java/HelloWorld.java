f80ef13780d498abb72cd38982d5bde5352fe379
Captianrock <alton.kim1@gmail.com>
2016-02-10 04:35:32 -0500

    Commiting the Java Hello World code!

---
+++ Code/Java/Software Performance/src/com/androidpv/java/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.androidpv.java;
+
+/**
+ * Created by kim on 2/10/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
