cc0fdbfe26fe05e03121e8ef58327186f760dda6
Bradley Blount <b.e.brad94@gmail.com>
2016-02-14 17:23:13 -0500

    Can parse through current directory and list off methods and their parent class

---
+++ Code/Java/Software Performance/src/com/androidpv/java/Parser.java
@@ -0,0 +1,89 @@
+package com.androidpv.java;
+
+/**
+ * Created by bradley on 2/12/2016.
+ */
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.eclipse.jdt.core.dom.*;
+
+public class Parser {
+
+    //use ASTParse to parse string
+    public static void parse(String str) {
+        ASTParser parser = ASTParser.newParser(AST.JLS3);
+        parser.setSource(str.toCharArray());
+        parser.setKind(ASTParser.K_COMPILATION_UNIT);
+        final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+        cu.accept(new ASTVisitor() {
+
+            Set names = new HashSet();
+
+            public boolean visit(MethodDeclaration node) {
+                SimpleName name = node.getName();
+                List classes = cu.types();
+                TypeDeclaration typeDec = (TypeDeclaration) classes.get(0);
+                //System.out.println("This is Class: " + typeDec.getName()  +  " and method: " + name);
+                this.names.add(name.getIdentifier());
+                return false; // do not continue
+            }
+
+
+            public boolean visit(SimpleName node) {
+                if (this.names.contains(node.getIdentifier())) {
+                    System.out.println("Usage of '" + node + "' at line "
+                            + cu.getLineNumber(node.getStartPosition()));
+                }
+                return true;
+            }
+        });
+    }
+
+    //read file content into a string
+    public static String readFileToString(String filePath) throws IOException {
+        StringBuilder fileData = new StringBuilder(1000);
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+
+        char[] buf = new char[10];
+        int numRead = 0;
+        while ((numRead = reader.read(buf)) != -1) {
+            //System.out.println(numRead);
+            String readData = String.valueOf(buf, 0, numRead);
+            fileData.append(readData);
+            buf = new char[1024];
+        }
+
+        reader.close();
+
+        return  fileData.toString();
+    }
+
+    //loop directory to get file list
+    public static void ParseFilesInDir() throws IOException{
+        File dirs = new File(".");
+        String dirPath = dirs.getCanonicalPath() + File.separator+"src"+File.separator;
+
+        File root = new File(dirPath);
+        //System.out.println(rootDir.listFiles());
+        File[] files = root.listFiles ( );
+        String filePath = null;
+        int n=0;
+        for (File f : files ) {
+            System.out.println(f);
+            filePath = f.getAbsolutePath();
+            if(f.isFile()){
+                System.out.println("FILE BEING PARSED" + f);
+                parse(readFileToString(filePath));
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ParseFilesInDir();
+    }
+}
