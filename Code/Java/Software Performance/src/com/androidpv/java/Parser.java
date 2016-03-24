16036b1da0ebcb465c8f948a77af999b63dba976
Captianrock <alton.kim1@gmail.com>
2016-03-24 07:21:56 -0400

    Fixing a commit
    Original commit : New GUI format to introduce File Directory by Bradley

--- Code/Java/Software Performance/src/com/androidpv/java/Parser.java
+++ Code/Java/Software Performance/src/com/androidpv/java/Parser.java
@@ -14,20 +14,18 @@ import java.util.Set;
 
 import com.androidpv.java.apkParser.APKParser;
 import com.androidpv.java.gui.PVGUI;
+import com.androidpv.java.gui.PVView;
 import com.androidpv.java.xposed.ModuleBuilder;
 import jadx.core.utils.exceptions.JadxException;
 import org.eclipse.jdt.core.dom.*;
 
 public class Parser {
 
-    //use ASTParse to parse string
+    //use ASTParse to parse files in given directory
     public static void parse(String str, String outputFile) {
-//    public static void parse(File file, String outputFile, String inputString) {
-
         ASTParser parser = ASTParser.newParser(AST.JLS3);
 
         parser.setSource(str.toCharArray());
-     //   parser.setSource(JavaCore.createCompilationUnitFrom((IFile) file));
         parser.setKind(ASTParser.K_COMPILATION_UNIT);
 
         parser.setResolveBindings(true);
@@ -39,32 +37,13 @@ public class Parser {
             Set names = new HashSet();
 
             public boolean visit(MethodDeclaration node) {
-//            public boolean visit(MethodDeclaration node) {
                 SimpleName name = node.getName();
                 List classes = cu.types();
                 TypeDeclaration typeDec = (TypeDeclaration) classes.get(0);
-                //System.out.println((cu.getPackage() != null ? cu.getPackage().getName().toString() : "Null")
-                //+ "," + typeDec.getName().toString() +  "," + name.toString());
-
-//                for (Object parameter : node.parameters()) {
-//                    VariableDeclaration variableDeclaration = (VariableDeclaration) parameter;
-//
-//                    String type = variableDeclaration.getStructuralProperty(SingleVariableDeclaration.TYPE_PROPERTY)
-//                            .toString();
-//                    System.out.println(type);
-//                    for (int i = 0; i < variableDeclaration.getExtraDimensions(); i++) {
-//                        type += "[]";
-//                    }
-//                }
-
 
                 printtoFile(outputFile, (cu.getPackage() != null ? cu.getPackage().getName().toString() : "Null") +
                         ";" + typeDec.getName().toString() + ";" + name.toString() + ";" + node.parameters() + ";"
                         + node.modifiers());
-//                + ","
-//                        + (!node.parameters().isEmpty() ? node.parameters().get(0).getClass() : "Null"));
-           //     System.out.println(node.typeParameters());
-
 
                 this.names.add(name.getIdentifier());
                 return false; // do not continue
@@ -89,12 +68,11 @@ public class Parser {
     }
 
     //loop directory to get file list
-    public static void ParseFilesInDir(List<File> files, String outputFile) {
+    public static void parseFilesInDir(List<File> files, String outputFile) {
         String filePath;
         for (File f : files) {
             filePath = f.getAbsolutePath();
             if (f.isFile()) {
-//                System.out.println("FILE BEING PARSED" + f);
                 try {
                     parse(readFileToString(filePath), outputFile);
                 } catch (Exception e) {
@@ -106,6 +84,7 @@ public class Parser {
         System.out.println("Done Parsing Files!");
     }
 
+    //Gets files in a given directory
     public static List getFiles(String input) {
         Path fp = Paths.get(input);
         PrintFiles pf = new PrintFiles();
@@ -135,65 +114,65 @@ public class Parser {
     }
 
     public static void main(String[] args) {
-        APKParser apkParser = new APKParser();
-        PVGUI gui = new PVGUI();
-        gui.createGUI();
-        List<File> fileL;
-        while (!gui.returnButtonPressed()) {
-            try {
-                Thread.currentThread().sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        String inputPathString = gui.getInputPath().trim();
-        String outputPathString = gui.getOutputPath().trim();
-
-        //Checks if the path is an apk file and automatically parsing for methods!
-        Path inputPath = Paths.get(inputPathString);
-        if (APKParser.isAPK(inputPath)) try {
-            System.out.println("Parsing APK NOW");
-            apkParser.parse(inputPath.toFile());
-            //C:\Users\bradley\IdeaProjects\android_PV\Code\Java\Software Performance\src\BBCNews.apk
-
-            fileL = getFiles(new File("").getAbsoluteFile().toString() + "/decompiledSource");
-            ParseFilesInDir(fileL, outputPathString);
-
-        } catch (JadxException e) {
-            System.err.println("Error JadxException in main: " + e.getMessage());
-            e.printStackTrace();
-        }
-
-
-        //
-        fileL = getFiles(inputPathString);
-        while (fileL == null) { // bad input
-            gui.resetGUI();
-            while (!gui.returnButtonPressed()) {
-                try {
-                    Thread.currentThread().sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            // reset strings and check if directory is valid
-            inputPathString = gui.getInputPath().trim();
-            outputPathString = gui.getOutputPath().trim();
-
-            fileL = getFiles(inputPathString);
-        }
-
-        try {
-            ParseFilesInDir(fileL, outputPathString);
-        } catch (Exception e) {
-            System.err.println("Error parseFilesInDir in main: " + e.getMessage());
-            e.printStackTrace();
-        }
-
-        System.out.println("Done");
-        gui.closeGUI();
-        ModuleBuilder moduleBuilder = new ModuleBuilder(outputPathString);
+//        APKParser apkParser = new APKParser();
+//        PVGUI gui = new PVGUI();
+//        gui.createGUI();
+//        List<File> fileL;
+//        while (!gui.returnButtonPressed()) {
+//            try {
+//                Thread.currentThread().sleep(1000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//
+//        String inputPathString = gui.getInputPath().trim();
+//        String outputPathString = gui.getOutputPath().trim();
+//
+//        //Checks if the path is an apk file and automatically parsing for methods!
+//        Path inputPath = Paths.get(inputPathString);
+//        if (APKParser.isAPK(inputPath)) try {
+//            System.out.println("Parsing APK NOW");
+//            apkParser.parse(inputPath.toFile());
+//            //C:\Users\bradley\IdeaProjects\android_PV\Code\Java\Software Performance\src\BBCNews.apk
+//
+//            fileL = getFiles(new File("").getAbsoluteFile().toString() + "/decompiledSource");
+//            parseFilesInDir(fileL, outputPathString);
+//
+//        } catch (JadxException e) {
+//            System.err.println("Error JadxException in main: " + e.getMessage());
+//            e.printStackTrace();
+//        }
+//
+//
+//        //
+//        fileL = getFiles(inputPathString);
+//        while (fileL == null) { // bad input
+//            gui.resetGUI();
+//            while (!gui.returnButtonPressed()) {
+//                try {
+//                    Thread.currentThread().sleep(1000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//
+//            // reset strings and check if directory is valid
+//            inputPathString = gui.getInputPath().trim();
+//            outputPathString = gui.getOutputPath().trim();
+//
+//            fileL = getFiles(inputPathString);
+//        }
+//
+//        try {
+//            parseFilesInDir(fileL, outputPathString);
+//        } catch (Exception e) {
+//            System.err.println("Error parseFilesInDir in main: " + e.getMessage());
+//            e.printStackTrace();
+//        }
+//
+//        System.out.println("Done");
+//        gui.closeGUI();
+        new PVView();
     }
 }
