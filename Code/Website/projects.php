b9bf9cd5ef6d2d8cc7055317ce15a8086046348a
Bradley Blount <b.e.brad94@gmail.com>
2016-03-29 20:08:10 -0400

    Pushing the Header file to use for all web pages and base applications page

---
+++ Code/Website/projects.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Software Engineering</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+ 	<?php include ('header.html'); ?>
+ 	<input name="Button" type="button" id="submit" onclick="test.php?runFunction=hello" value="Button" />
+
+  </body>
+  </html>
