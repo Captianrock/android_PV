807c906804f8566632e2737430aaffa847041a71
Bradley Blount <b.e.brad94@gmail.com>
2016-03-30 08:42:01 -0400

    Added the alarmklock page (static for now)

---
+++ Code/Website/alarmklock.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Software Engineering</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+ 	  <?php include ('header.html'); ?>
+
+    <center>
+      <font size="6" color="white">Application: AlarmKlock</font>
+    </center>
+    
+    <center>
+      <img src="images/alarmklock.png" alt="Mountain View" ;>
+    </center>
+
+    <div class="dropdown">
+      <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown" align="left"> Trace Select 
+      <span class="caret"></span></button>
+      <ul class="dropdown-menu">
+        <li><a href="#">Trace 1</a></li>
+        <li><a href="#">Trace 2</a></li>
+        <li><a href="#">Trace 3</a></li>
+      </ul>
+    </div>
+
+ 	<!-- Drop down menu populated with the names of applicaton in database
+ 	 -->
+
+	
+  </body>
+  </html>
