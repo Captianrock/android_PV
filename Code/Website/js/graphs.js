9abe098cfb658c70ee6db46f0231563cb61042e0
Captianrock <alton.kim1@gmail.com>
2016-04-08 01:24:25 -0400

    High Charts Updated
    -High Charts now include an accurate Donut Chart.
    -ISSUE: Colors are not correct yet

--- Code/Website/js/graphs.js
+++ Code/Website/js/graphs.js
@@ -3,6 +3,7 @@
 console.log(jsArray);
 
 var method = [];
+var donutDic = {};
 var start = [];
 var end = [];
 var diff = [];
@@ -32,47 +33,106 @@ for (var i = 0; i < jsArray.length; i++)
             maxMethod = jsArray[i]["methodName"];
         }
     }
+    if(jsArray[i]["methodName"] in donutDic){
+        var temp = (donutDic[jsArray[i]["methodName"]]);
+        if(jsArray[i]["methodName"] == "toString"){
+            var temp = [];
+            temp.push(parseInt(jsArray[i]["timeEnd"])- parseInt(jsArray[i]["timeStart"]));
+            donutDic[jsArray[i]["methodName"]] = temp;
+        }
+        else{
+            temp.push(parseInt(jsArray[i]["timeEnd"])- parseInt(jsArray[i]["timeStart"]));
+            donutDic[jsArray[i]["methodName"]] = temp;
+        }
+    }
+    else{
+        var temp = [];
+        temp.push(parseInt(jsArray[i]["timeEnd"])- parseInt(jsArray[i]["timeStart"]));
+        donutDic[jsArray[i]["methodName"]] = temp;
+        
+    }
 }
-for (var i = 0; i < method.length; i++) 
+//Create Dynamic Data for Donut Graph
+var colors = Highcharts.getOptions().colors;
+for (i = 0; i < method.length; i++) 
 {
     pie.push({
-        name: method[i],
-        y: diff[i]
+        y: diff[i],
+        drilldown: {
+            categories: method[i],
+            data: donutDic[method[i]],
+            color: colors[i]
+        }
     });
 }
-
 $(function () {
-    $(document).ready(function () {
-        // Build the chart
-        $('#container').highcharts({
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false,
-                type: 'pie'
-            },
-            title: {
-            text: 'Method Runtime Percentages'
-            },
-            tooltip: {
-                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: false
-                    },
-                    showInLegend: true
-                }
-            },
-            series: [{
-                name: 'Brands',
-                colorByPoint: true,
-                data: pie
-            }]
+
+    categories = method;
+    data = pie;
+    browserData = [];
+    versionsData = [];
+    dataLen = data.length;
+
+    // Build the data arrays
+    for (var i = 0; i < dataLen; i += 1) {
+
+        // add browser data
+        browserData.push({
+            name: categories[i],
+            y: data[i].y,
+            color: data[i].color
         });
+
+        // add version data
+        drillDataLen = data[i].drilldown.data.length;
+        for (var j = 0; j < drillDataLen; j += 1) {
+            brightness = 0.2 - (j / drillDataLen) / 5;
+            versionsData.push({
+                name: data[i].drilldown.categories,
+                y: data[i].drilldown.data[j],
+                color: Highcharts.Color(data[i].color).brighten(brightness).get()
+            });
+        }
+    }
+
+    // Create the chart
+    $('#container').highcharts({
+        chart: {
+            type: 'pie'
+        },
+        title: {
+            text: 'Method Runtimes'
+        },
+        yAxis: {
+            title: {
+                text: 'Total percent market share'
+            }
+        },
+        plotOptions: {
+            pie: {
+                shadow: false,
+                center: ['50%', '50%'],
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false
+                }
+            }
+        },
+        series: [{
+            name: 'Browsers',
+            data: browserData,
+            size: '55%',
+            colorByPoint: true,
+           
+        }, {
+            name: 'Versions',
+            data: versionsData,
+            size: '90%',
+            innerSize: '70%',
+           // colorByPoint: true,
+            
+        }]
     });
 });
 
