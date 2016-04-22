579a5fc4ca3c15f8f583b0ed5d899e2a8b0c4fc7
Quansword <Quanbook@gmail.com>
2016-04-22 05:41:47 -0400

    Fixed Alton's fix for the bug he caused
    - Dreams crushed
    - Alton definitely did this on purpose
    - Crying now.

--- Code/Website/barChart.php
+++ Code/Website/barChart.php
@@ -194,7 +194,7 @@ $userName = $_SESSION['user'];
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Charts
+                            <?php echo $_SESSION['app'] ?> <small>Bar Chart</small>
                         </h1>
                         <ol class="breadcrumb">
                             <li>
--- Code/Website/tables.php
+++ Code/Website/tables.php
@@ -192,7 +192,7 @@ $userName = $_SESSION['user'];
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Tables
+                            <?php echo $_SESSION['app'] ?> <small>Tables</small>
                         </h1>
                         <ol class="breadcrumb">
                             <li>
